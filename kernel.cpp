51f3d1ec6439b2982e1eebb07c30690a08c4cfee
danielyang21 <danielyanxiaoyang21@gmail.com>
2025-09-24 01:29:07 +0000

    testing

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,7 @@
-void kernel_main() {
+#include <cstdio>
+void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
     printf("Hello world!");
+    printf("%d", magicnumber);
 
     while(1);
 }
---
+++ loader.s
@@ -0,0 +1,30 @@
+.set MAGIC, 0x1bab002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+
+.section .text
+.extern kernelMain
+.global loader
+
+loader:
+    mov $kernel_stack, %esp
+    push %eax
+    push %ebx
+    call kernelMain
+
+
+_stop:
+    cli
+    hlt
+    jmp_stop # another infinite loop
+
+
+.section .bss
+.space 2*1024*1024 # 2 MiB
+kernel_stack:
